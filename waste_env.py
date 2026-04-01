9aaf6b5c55ff5ff7ff3e0c66427f5ecdcfca429c
srinandan0039-eng <srinandan0039@gmail.com>
2026-04-01 17:21:52 +0530

    Delete waste_env.py

--- waste_env.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-import random
-
-def run():
-    areas = [random.randint(20,80) for _ in range(3)]
-    return f"Garbage levels: {areas}"
-
-gr.Interface(fn=run, inputs=[], outputs="text").launch()
